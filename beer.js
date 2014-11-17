ed0454532bdac81c8ca34fae67f01d4aa90949f3
alessandro-allen <alessandro.lee.allen@gmail.com>
2014-11-17 18:52:02 -0500

    Updated keyword list
    Also added a function to generate the regEx pattern from the keyword
    list

--- beer.js
+++ beer.js
@@ -1,8 +1,10 @@
-/**
- * Created by dromagnum on 11/12/14.
- */
+
 var totalTags = 3;
 var tags_dict = new Array();
+//Temp seasonal values as for prototyping
+var styleEffect = 2;
+
+//example user dict of tags
 tags_dict["hoppy"] = 1;
 tags_dict["fruit"] = 0;
 tags_dict["citrus"] = 2;
@@ -52,15 +54,38 @@ var calculatePlaylist = function(beers, maxDepth){
     }
     return playlist;
 }
-var transfer = function(beer){
+var transfer = function(beer, style){
     var sum = 0;
     var numTags = beer.tags.length();
     for( var i = 0; i < numTags; i++){
         sum += (tags_dict[beer.tags[i]]) * (1/totalTags);
+        if(beer.style == style){
+           sum *= styleEffect;
+        }
+
     }
     return sum
 }
 
 var discount = function(beer, depth){
     return beer.abv*depth;
+}
+
+var regex_pattern = function( inputString ){
+    var list = inputString.split("\n");
+    var regExString = "/w*\s*";
+    for(var i = 0; i < list.length(); i++){
+        if( i == 0){
+            regExString += list[i];
+        }
+        else {
+            regExString += "|" + list[i];
+        }
+    }
+    regExString += "\s+/i";
+    return regExString;
+}
+
+var extend_tag_list(beer, ){
+
 }
